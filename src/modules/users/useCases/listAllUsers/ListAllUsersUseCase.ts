a9400138eb342b99fb5b23b707139448a5109951
bentopedro <63908808+bentopedro@users.noreply.github.com>
2022-05-23 12:24:10 +0100

    ListAllUsers

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,15 +2,22 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User[] {
+        const adminUser = this.usersRepository.findById(user_id);
+
+        if (adminUser.admin === false) {
+            throw new Error("USer is not admin");
+        }
+
+        const users = this.usersRepository.list();
+        return users;
+    }
 }
 
 export { ListAllUsersUseCase };
